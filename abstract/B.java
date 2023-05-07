c40c33f114f2bbc5c73d0ff058e490d04c078867
perinbaraj777 <perinbaraj777@gmail.com>
2023-05-07 23:39:43 +0530

    abstract method

---
+++ abstract/B.java
@@ -0,0 +1,30 @@
+class A{
+static int aa=80;
+int c=60;
+public static void m1(){
+
+
+
+}
+static int m2(int x){
+return x;
+}
+void m3(){
+
+}
+
+}
+
+class B{
+	static void m5(){
+
+}
+void m6(){
+}
+
+
+public static void main(String[] args){
+System.out.println(A.aa);
+
+}
+}
